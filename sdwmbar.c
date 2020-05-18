d4d198014cbd6edfcb6f7329df8ee88294c7d71b
Sergei Akhmatdinov <sakhmatd@darkn.space>
2020-05-17 21:39:59 -0400

    fix trailing whitespace, add GTAGS to gitignore

--- sdwmbar.c
+++ sdwmbar.c
@@ -1,15 +1,15 @@
-/* Copyright 2020 Sergei Akhmatdinov                                         */
-/*                                                                           */
-/* Licensed under the Apache License, Version 2.0 (the "License");           */
-/* you may not use this file except in compliance with the License.          */
-/* You may obtain a copy of the License at                                   */
-/*                                                                           */
-/*     http://www.apache.org/licenses/LICENSE-2.0                            */
-/*                                                                           */
-/* Unless required by applicable law or agreed to in writing, software       */
-/* distributed under the License is distributed on an "AS IS" BASIS,         */
+/* Copyright 2020 Sergei Akhmatdinov					     */
+/*									     */
+/* Licensed under the Apache License, Version 2.0 (the "License");	     */
+/* you may not use this file except in compliance with the License.	     */
+/* You may obtain a copy of the License at				     */
+/*									     */
+/*     http://www.apache.org/licenses/LICENSE-2.0			     */
+/*									     */
+/* Unless required by applicable law or agreed to in writing, software	     */
+/* distributed under the License is distributed on an "AS IS" BASIS,	     */
 /* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  */
-/* See the License for the specific language governing permissions and       */
+/* See the License for the specific language governing permissions and	     */
 /* limitations under the License. */
 
 /* BSD definitions take priority. */
@@ -42,7 +42,7 @@
 #define AC_LEN 2
 
 void
-printerr(char *err) 
+printerr(char *err)
 {
 	fprintf(stderr, "%s\n", err);
 	exit(1);
@@ -52,11 +52,11 @@ void
 getversion(char *buff)
 {
 	FILE *fp;
-	
+
 	fp = popen("dwm -v 2>&1", "r");
 	if (!fp)
 		printerr("sdwmbar: no dwm binary found!");
-	
+
 	fgets(buff, VRSN_LEN, fp);
 	fclose(fp);
 
@@ -71,7 +71,7 @@ gettime(char *buff)
 	struct tm *tm = localtime(&t);
 	if (!tm)
 		printerr("sdwmbar: unable to get time (localtime)!");
-	
+
 	if (!strftime(buff, TIME_LEN, "%D  %R", tm))
 		printerr("sdwmbar: unable to get time (strftime)!");
 }
@@ -81,7 +81,7 @@ getbatt(int *buff)
 {
 	size_t len = sizeof(buff);
 	int ret = sysctlbyname("hw.acpi.battery.life", buff, &len, NULL, 0);
-	
+
 	if (ret < 0) {
 		fprintf(stderr, "WARN: No battery was detected.\n");
 		return 0;
@@ -96,7 +96,7 @@ getac(char *buff)
 	int ac;
 	size_t len = sizeof(ac);
 	sysctlbyname("hw.acpi.acline", &ac, &len, NULL, 0);
-	
+
 	*buff = (ac) ? '+' : '\0';
 
 	/* Ensure null termination */
@@ -110,8 +110,9 @@ getload(char *buff)
 
 	if (getloadavg(results, 3) < 0)
 		printerr("sdwmbar: unable to get load data!");
-       
-	snprintf(buff, LOAD_LEN, "%.2f %.2f %.2f", results[0], results[1], results[2]);
+
+	snprintf(buff, LOAD_LEN, "%.2f %.2f %.2f", results[0], results[1],
+		 results[2]);
 }
 
 void
@@ -121,7 +122,7 @@ setstatus(char *status)
 
 	if (!(display = XOpenDisplay(0)))
 		printerr("sdwmbar: unable to open display!");
-	
+
 	XStoreName(display, DefaultRootWindow(display), status);
 	XSync(display, 0);
 	XCloseDisplay(display);
@@ -147,10 +148,10 @@ main(void)
 		/* getbatt() returns 0 if no battery exists */
 		if (getbatt(&batt)) {
 			getac(ac);
-			snprintf(status, STAT_LEN, "%s  L:%s  %s  %s%d%%",
+			snprintf(status, STAT_LEN, "%s	L:%s  %s  %s%d%%",
 					version, load, time, ac, batt);
 		} else {
-			snprintf(status, 100, "%s  L:%s  %s",
+			snprintf(status, 100, "%s  L:%s	 %s",
 					version, load, time);
 		}
 
