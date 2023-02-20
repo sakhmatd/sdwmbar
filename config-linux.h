2789e00a7477444f3e3bf36cd31edf9bf5050e6b
Ivan Kovmir <i@kovmir.eu>
2023-02-20 21:59:30 +0100

    Add sdwmbar-linux.c to include Linux support
    To avoid #ifdef hell, Linux support has been included within a separate
    source file.
    
    To build the Linux version, use GNUMakefile like `make -f GNUMakefile`.

---
+++ config-linux.h
@@ -0,0 +1,5 @@
+/* Path to the AC plug. */
+static const char *PLUG_PATH = "/sys/class/power_supply/AC/online";
+
+/* Path to the battery. */
+static const char *BATT_PATH = "/sys/class/power_supply/BAT0/capacity";
